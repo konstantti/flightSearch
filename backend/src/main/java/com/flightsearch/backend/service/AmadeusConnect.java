e8ff13c0265798cf6c15ec18c089d954adee2dee
t1juko00 <t1juko00@students.oamk.fi>
2024-01-10 10:26:27 +0200

    lisää toiminnallisuutta

--- backend/src/main/java/com/flightsearch/backend/service/AmadeusConnect.java
+++ backend/src/main/java/com/flightsearch/backend/service/AmadeusConnect.java
@@ -1,9 +1,12 @@
 package com.flightsearch.backend.service;
-
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.referenceData.Locations;
+import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.FlightOrder;
+import com.amadeus.resources.FlightPrice;
 import com.amadeus.resources.Location;
+import com.google.gson.JsonObject;
 import com.amadeus.exceptions.ResponseException;
 
 public enum AmadeusConnect {
@@ -11,7 +14,7 @@ public enum AmadeusConnect {
     private Amadeus amadeus;
     private AmadeusConnect() {
         this.amadeus = Amadeus
-            .builder("QKtVpo63QvQJiSFkAcdsOLq7Civ9IbZz", "5G6d3mId5uTNzzap")
+            .builder("", "")
             .build();
     }
     
@@ -20,4 +23,22 @@ public enum AmadeusConnect {
             .with("keyword", keyword)
             .and("subType", Locations.AIRPORT));
     }
+
+    public FlightOfferSearch[] flights(String origin, String destination, String departDate, String adults, String returnDate) throws ResponseException {
+        return amadeus.shopping.flightOffersSearch.get(
+                  Params.with("originLocationCode", origin)
+                          .and("destinationLocationCode", destination)
+                          .and("departureDate", departDate)
+                          .and("returnDate", returnDate)
+                          .and("adults", adults)
+                          .and("max", 3));
+    }
+
+    public FlightPrice confirm(FlightOfferSearch offer) throws ResponseException {
+    return amadeus.shopping.flightOffersSearch.pricing.post(offer);
+}
+
+    public FlightOrder order(JsonObject order) throws ResponseException {
+    return amadeus.booking.flightOrders.post(order);
+}
 }
